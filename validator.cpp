856075a3e618bbdc1ac34b8e491f38dcf646d841
低熵体 <16698219+dishangti@users.noreply.github.com>
2023-04-09 12:27:45 +0800

    Add files via upload

---
+++ validator.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <ctime>
+#include <mutex>
+
+using namespace std;
+
+mutex mtx;
+
+void check(uint64_t submin, uint64_t submax)
+{
+    clock_t start, end;
+    start = clock();
+    for (uint64_t i = submin; i <= submax; i++) {
+        uint64_t x = i;
+
+        while (x != 1) {
+            if (x & 1) x = 3 * x + 1;
+            else x >>= 1;
+        }
+    }
+
+    end = clock();
+    mtx.lock();
+    printf("%llu to %llu: Pass! %lf sec\n", submin, submax, double(end-start)/CLOCKS_PER_SEC);
+    mtx.unlock();
+    return;
+}
+
+int main()
+{
+    uint64_t mmin, mmax, step, submin, submax, nnum;
+    uint32_t thnum;
+    
+    printf("Min: "); scanf("%llu", &mmin);
+    printf("Max: "); scanf("%llu", &mmax);
+    printf("Threads: "); scanf("%u", &thnum);
+    nnum = mmax - mmin + 1;
+    if (thnum > nnum) thnum = nnum;
+
+    step = nnum / thnum;
+    submin = mmin;
+    for (uint32_t i = 1; i <= thnum; i++) {
+        if (i == thnum) submax = mmax;
+        else submax = submin + step - 1;
+
+        thread t(check, submin, submax);
+        t.detach();
+
+        submin = submax + 1;
+    }
+
+    fflush(stdin);
+    getchar();
+    return 0;
+}
